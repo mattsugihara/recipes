853c57ea2cc8af81e53498feaad9304f8a81c216
Matt Sugihara <mattsugihara@gmail.com>
2021-09-09 00:12:46 -0700

    update pages on request

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,46 @@
 ---
 layout: null
 ---
+var cacheName = "recipes";
+
 // List of files to cache
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open('recipes').then(function(cache) {
-      return cache.addAll([
-        '/recipes/',
-        '/recipes/css/main.css',
-        {% for post in site.posts %}
-        '/recipes{{ post.url }}',
-        {% endfor %}
-      ]);
-    })
+    precache()
   );
 });
 
 // Listens for fetch events and returns request from cache, if possible, and
 // otherwise sends out the request
 self.addEventListener('fetch', function(e) {
-  console.log('Request: ' + e.request.url);
-
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      if (response) {
-        console.log('Served from local: ' + e.request.url)
-        return response;
-      } else {
-        console.log('Served from remote: ' + e.request.url)
-        return fetch(e.request);
-      }
-    })
-  );
+  e.respondWith(fromCache(e.request));
+  e.waitUntil(update(e.request));
 });
+
+function precache() {
+  caches.open(cacheName).then(function(cache) {
+    return cache.addAll([
+      '/recipes/',
+      '/recipes/css/main.css',
+      {% for post in site.posts %}
+      '/recipes{{ post.url }}',
+      {% endfor %}
+    ]);
+  })
+}
+
+function fromCache(request) {
+  return caches.open(cacheName).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
+
+function update(request) {
+  return caches.open(cacheName).then(function (cache) {
+    return fetch(request).then(function (response){
+      return cache.put(request,response);
+    });
+  });
+}
