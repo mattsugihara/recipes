2c4ee64fec8dab23674c106df262269fb4fd4025
Matt Sugihara <mattsugihara@gmail.com>
2021-09-09 23:22:11 -0700

    improve offline

--- service-worker.js
+++ service-worker.js
@@ -10,13 +10,16 @@ self.addEventListener('install', function(e) {
   );
 });
 
-// Listens for fetch events and returns request from cache, if possible, and
-// otherwise sends out the request
+// Listens for fetch events
+// calls fromCache to either respod from the cache (or fail)
+// then calls the server to update the cache
 self.addEventListener('fetch', function(e) {
   e.respondWith(fromCache(e.request));
   e.waitUntil(update(e.request));
 });
 
+
+// caches files for future use
 function precache() {
   caches.open(cacheName).then(function(cache) {
     return cache.addAll([
@@ -29,18 +32,28 @@ function precache() {
   })
 }
 
+// Responds to requests with asset from cache or falls back to requesting the
+// asset from the networ
 function fromCache(request) {
   return caches.open(cacheName).then(function (cache) {
     return cache.match(request).then(function (matching) {
-      return matching || Promise.reject('no-match');
+      return matching || fetch(request);
     });
   });
 }
 
+// Updates assets in cache
+// If the extension of the request URL is empty (i.e. html), js, or css, then
+// this updates the asset in cache.
 function update(request) {
-  return caches.open(cacheName).then(function (cache) {
-    return fetch(request).then(function (response){
-      return cache.put(request,response);
+  const url = new URL (request.url);
+  const extension = url.pathname.split('.').pop();
+
+  if (( '' == extension || 'css' == extension) || 'js' == extension ) {
+    return caches.open(cacheName).then(function (cache) {
+      return fetch(request).then(function (response){
+        return cache.put(request,response);
+      });
     });
-  });
+  }
 }
